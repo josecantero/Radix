700eefadfe0f1bfba7b21687644b1bd6e3e2c06b
kabudev <kabudev@gmail.com>
2025-10-06 00:03:13 -0300

    modularization implementation in main

---
+++ build/CMakeFiles/radix_blockchain.dir/DependInfo.cmake
@@ -0,0 +1,31 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/media/kabudev/Seagate Expansion Drive/Radix/src/base58.cpp" "CMakeFiles/radix_blockchain.dir/src/base58.cpp.o" "gcc" "CMakeFiles/radix_blockchain.dir/src/base58.cpp.o.d"
+  "/media/kabudev/Seagate Expansion Drive/Radix/src/block.cpp" "CMakeFiles/radix_blockchain.dir/src/block.cpp.o" "gcc" "CMakeFiles/radix_blockchain.dir/src/block.cpp.o.d"
+  "/media/kabudev/Seagate Expansion Drive/Radix/src/blockchain.cpp" "CMakeFiles/radix_blockchain.dir/src/blockchain.cpp.o" "gcc" "CMakeFiles/radix_blockchain.dir/src/blockchain.cpp.o.d"
+  "/media/kabudev/Seagate Expansion Drive/Radix/src/crypto.cpp" "CMakeFiles/radix_blockchain.dir/src/crypto.cpp.o" "gcc" "CMakeFiles/radix_blockchain.dir/src/crypto.cpp.o.d"
+  "/media/kabudev/Seagate Expansion Drive/Radix/src/main.cpp" "CMakeFiles/radix_blockchain.dir/src/main.cpp.o" "gcc" "CMakeFiles/radix_blockchain.dir/src/main.cpp.o.d"
+  "/media/kabudev/Seagate Expansion Drive/Radix/src/merkle_tree.cpp" "CMakeFiles/radix_blockchain.dir/src/merkle_tree.cpp.o" "gcc" "CMakeFiles/radix_blockchain.dir/src/merkle_tree.cpp.o.d"
+  "/media/kabudev/Seagate Expansion Drive/Radix/src/money_util.cpp" "CMakeFiles/radix_blockchain.dir/src/money_util.cpp.o" "gcc" "CMakeFiles/radix_blockchain.dir/src/money_util.cpp.o.d"
+  "/media/kabudev/Seagate Expansion Drive/Radix/src/randomx_util.cpp" "CMakeFiles/radix_blockchain.dir/src/randomx_util.cpp.o" "gcc" "CMakeFiles/radix_blockchain.dir/src/randomx_util.cpp.o.d"
+  "/media/kabudev/Seagate Expansion Drive/Radix/src/transaction.cpp" "CMakeFiles/radix_blockchain.dir/src/transaction.cpp.o" "gcc" "CMakeFiles/radix_blockchain.dir/src/transaction.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/radix_blockchain.dir/cmake_clean.cmake
@@ -0,0 +1,27 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/radix_blockchain.dir/src/base58.cpp.o"
+  "CMakeFiles/radix_blockchain.dir/src/base58.cpp.o.d"
+  "CMakeFiles/radix_blockchain.dir/src/block.cpp.o"
+  "CMakeFiles/radix_blockchain.dir/src/block.cpp.o.d"
+  "CMakeFiles/radix_blockchain.dir/src/blockchain.cpp.o"
+  "CMakeFiles/radix_blockchain.dir/src/blockchain.cpp.o.d"
+  "CMakeFiles/radix_blockchain.dir/src/crypto.cpp.o"
+  "CMakeFiles/radix_blockchain.dir/src/crypto.cpp.o.d"
+  "CMakeFiles/radix_blockchain.dir/src/main.cpp.o"
+  "CMakeFiles/radix_blockchain.dir/src/main.cpp.o.d"
+  "CMakeFiles/radix_blockchain.dir/src/merkle_tree.cpp.o"
+  "CMakeFiles/radix_blockchain.dir/src/merkle_tree.cpp.o.d"
+  "CMakeFiles/radix_blockchain.dir/src/money_util.cpp.o"
+  "CMakeFiles/radix_blockchain.dir/src/money_util.cpp.o.d"
+  "CMakeFiles/radix_blockchain.dir/src/randomx_util.cpp.o"
+  "CMakeFiles/radix_blockchain.dir/src/randomx_util.cpp.o.d"
+  "CMakeFiles/radix_blockchain.dir/src/transaction.cpp.o"
+  "CMakeFiles/radix_blockchain.dir/src/transaction.cpp.o.d"
+  "radix_blockchain"
+  "radix_blockchain.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/radix_blockchain.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
